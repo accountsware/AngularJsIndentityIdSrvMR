5775b1fd119cf4c0e13886e8c3694c44238dbc74
accountsware <muhammad@accountsware.com>
2015-04-14 16:39:15 +0100

    Web.mvc

--- Angular.Data/Migrations/Configuration.cs
+++ Angular.Data/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 
 
 
+using System.Collections.Generic;
+using Angular.Core.Modals;
 using Angular.Data.Context;
 
 namespace Angular.Data.Migrations
@@ -23,10 +25,37 @@ namespace Angular.Data.Migrations
         protected override void Seed(AngularContext context)
         {
 
-            
+            var customers = new List<Customer>();
 
+
+
+
+            customers.Add(new Customer()
+            {
+                CompanyName = "Company1",
+                ContactTitle = "ContactTitle1",
+                Country = "Country1",
+                Fax = "Fax1"
+            });
+            customers.Add (new Customer(){CompanyName = "Company2", ContactTitle = "ContactTitle2", Country = "Country2", Fax = "Fax2"
+            })
+            ;
+            customers.Add(new Customer()
+            {
+                CompanyName = "Company3",
+                ContactTitle = "ContactTitle3",
+                Country = "Country3",
+                Fax = "Fax3"
+            });
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+        }
+
+
+            
 
 
         }
     }
-}
+
