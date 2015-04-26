76f6a53ea5f4b9f4d71856664aae85a2acde31eb
accountsware <muhammad@accountsware.com>
2015-04-26 12:28:57 +0100

    Userservice ext

---
+++ Angular.AuthServer/UserManagementExtension/GuidExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Angular.AuthServer.UserManagementExtension
+{
+    public static class GuidExt
+    {
+    public static Guid ToGuid(this string s)
+        {
+            Guid g;
+            if (!String.IsNullOrWhiteSpace(s) &&
+                Guid.TryParse(s, out g))
+            {
+                return g;
+            }
+            
+            return Guid.Empty;
+        }
+    }
+}
